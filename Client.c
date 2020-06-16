a5ef264eae6f22d902fccc95291b374c101fbfaa
OrestisStefanou <orestaras1997@outlook.com>
2020-06-16 15:58:30 +0300

    Client progressing

--- Client.c
+++ Client.c
@@ -15,7 +15,7 @@ void *thread_function(void *);  //Declare thread function
 char server_name[SERVER_NAME_LEN_MAX + 1] = { 0 };
 int server_port;
 
-int time_to_go=0,thread_number;
+int finished=0,thread_number;
 
 int main(int argc, char const *argv[])
 {   
@@ -41,8 +41,6 @@ int main(int argc, char const *argv[])
 
     pthreads = malloc(thread_number * sizeof(pthread_t));    //Create the array with pthread's id
 
-    //sleep(1);
-
     requests_array_size = thread_number;
     init_requests_array();
 
@@ -62,34 +60,36 @@ int main(int argc, char const *argv[])
     while(1)
     {
         for(int i=0;i<thread_number;i++){
-            if(fgets(buf,100,fp)){
                 //printf("Buf is %s",buf);
                 //Lock mutex
                 if(err=pthread_mutex_lock(&mtx)){
                     perror2("phtread_mutex_lock",err);
                     exit(EXIT_FAILURE);
-                }                
-                /* Create thread to serve connection to client. */
-                if (pthread_create(&pthreads[i], &pthread_attr, thread_function, (void *)buf) != 0) {
-                    perror("pthread_create");
-                    continue;
                 }
-                //printf("Main waiting\n");
-                pthread_cond_wait(&cvar,&mtx);
-                //sprintf("Main continues\n");
-                //Unlock mutex
-                if (err=pthread_mutex_unlock(&mtx))
-                {
-                    perror2("pthread mutex unlock\n",err);
-                    exit(EXIT_FAILURE);
-                }
-            }
+                if(fgets(buf,100,fp)){                
+                    /* Create thread to serve connection to client. */
+                    if (pthread_create(&pthreads[i], &pthread_attr, thread_function, (void *)buf) != 0) {
+                        perror("pthread_create");
+                        continue;
+                    }
+                    pthread_cond_wait(&cvar,&mtx);
+                    //Unlock mutex
+                    if (err=pthread_mutex_unlock(&mtx))
+                    {
+                        perror2("pthread mutex unlock\n",err);
+                        exit(EXIT_FAILURE);
+                    }
+                }   
             else{
-                flag=1;
+                flag=i;
                 break;
             }
+                
         }
-        if (flag==1)
+        for(int i=0;i<flag;i++){
+            pthread_join(pthreads[i],NULL);
+        }
+        if (flag>0)
         {
             break;
         }
@@ -115,77 +115,84 @@ int main(int argc, char const *argv[])
 void *thread_function(void *argp){
     int err;
     char request[100];
-            //Lock mutex
-            if(err=pthread_mutex_lock(&mtx)){
-                perror2("phtread_mutex_lock",err);
-                exit(EXIT_FAILURE);
-            }    
+    
+    //Lock mutex
+    if(err=pthread_mutex_lock(&mtx)){
+        perror2("phtread_mutex_lock",err);
+        exit(EXIT_FAILURE);
+    }
+
     char *r=(char *)argp;
     strcpy(request,r);
     //printf("Request is %s\n",request);
 
-            //pthread_cond_signal(&cvar);
+    pthread_cond_signal(&cvar);
 
-                //Unlock mutex
-            if (err=pthread_mutex_unlock(&mtx))
-            {
-                perror2("pthread mutex unlock\n",err);
-                exit(EXIT_FAILURE);
-            }
+    //Unlock mutex
+    if (err=pthread_mutex_unlock(&mtx))
+    {
+        perror2("pthread mutex unlock\n",err);
+        exit(EXIT_FAILURE);
+    }
     
-        int request_code = get_request_code(request);
-        if(request_code!=-1){
-            //Send the request
-            int socket_fd;
-            struct hostent *server_host;
-            struct sockaddr_in server_address;
+    int request_code = get_request_code(request);
+    if(request_code!=-1){
+        //Send the request
+        int socket_fd;
+        struct hostent *server_host;
+        struct sockaddr_in server_address;
 
-            // Get server host from server name.
-            server_host = gethostbyname(server_name);
+        // Get server host from server name.
+        server_host = gethostbyname(server_name);
 
-            // Initialise IPv4 server address with server host.
-            memset(&server_address, 0, sizeof server_address);
-            server_address.sin_family = AF_INET;
-            server_address.sin_port = htons(server_port);
-            memcpy(&server_address.sin_addr.s_addr, server_host->h_addr, server_host->h_length);
+        // Initialise IPv4 server address with server host.
+        memset(&server_address, 0, sizeof server_address);
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(server_port);
+        memcpy(&server_address.sin_addr.s_addr, server_host->h_addr, server_host->h_length);
 
-            // Create TCP socket.
-            if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-                perror("socket");
-                exit(1);
-            }
-
-            // Connect to socket with server address.
-            if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof server_address) == -1) {
-                perror("connect");
-                exit(1);
-            }
-
-            write(socket_fd,request,strlen(request));
-        
-            //Lock mutex
-            if(err=pthread_mutex_lock(&mtx)){
-                perror2("phtread_mutex_lock",err);
-                exit(EXIT_FAILURE);
-            }
-
-            printf("Request is %s\n",request);
-            //Get the response
-            char c;
-            while (read(socket_fd,&c,1))
-            {
-                printf("%c",c);
-            }
-
-            pthread_cond_signal(&cvar);
-            //Unlock mutex
-            if (err=pthread_mutex_unlock(&mtx))
-            {
-                perror2("pthread mutex unlock\n",err);
-                exit(EXIT_FAILURE);
-            }
-
-            close(socket_fd);
+        // Create TCP socket.
+        if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("socket");
+            exit(1);
         }
-    
+
+        // Connect to socket with server address.
+        /*if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof server_address) == -1) {
+            perror("connect");
+            exit(1);
+        }*/
+
+        //write(socket_fd,request,strlen(request));
+        
+        //Lock mutex
+        if(err=pthread_mutex_lock(&mtx)){
+            perror2("phtread_mutex_lock",err);
+            exit(EXIT_FAILURE);
+        }
+        // Connect to socket with server address.
+        if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof server_address) == -1) {
+            perror("connect");
+            exit(1);
+        }
+        write(socket_fd,request,strlen(request));
+        printf("Request is %s\n",request);
+        //Get the response
+        char c;
+        while (read(socket_fd,&c,1))
+        {
+            printf("%c",c);
+        }
+
+        //pthread_cond_signal(&cvar);
+        //Unlock mutex
+        if (err=pthread_mutex_unlock(&mtx))
+        {
+            perror2("pthread mutex unlock\n",err);
+            exit(EXIT_FAILURE);
+        }
+
+        close(socket_fd);
+    }
+return NULL;
 }
